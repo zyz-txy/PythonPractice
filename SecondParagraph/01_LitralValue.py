0dca44eef3e834d54f0c7d743f76906363e8392e
zyz-txy <1876195621@qq.com>
2026-07-08 14:50:49 +0800

    字面量与变量

--- SecondParagraph/01_LitralValue.py
+++ SecondParagraph/01_LitralValue.py
@@ -1,7 +1,19 @@
-print(100)
+
+num1 = 100
+print(num1)
+num1 = num1 + 1
+print(num1)
+num1 = "ok"
+print(num1)#一个变量可以存多个类型，但一般不会
+num2 = 3.14
 print(3.14)
-print("hello")
-print(False)
-print(True)
+num3 = "hello world"
+print(num3)
+num4 = True
+print(num4)
 print("----------")
 print(None)
+
+base,increase = 30.7,15.4#一次性定义多个变量
+print("下个月总量：",base+increase)#print多项数据之间逗号分隔
+print("下下个月总量：",base+increase+increase)
