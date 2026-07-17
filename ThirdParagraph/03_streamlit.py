01c2c332c992d6f57e30e2f97f0addb8ec4f23d9
zyz-txy <1876195621@qq.com>
2026-07-18 01:20:03 +0800

    streamlit基础

---
+++ ThirdParagraph/03_streamlit.py
@@ -0,0 +1,13 @@
+#streamlit是一个用于快速基于python代码构建web网页的python库（数据科学及机器学习领域）
+
+#安装streamlit（终端运行pip install streamlit）
+
+#在python文件中引入streamlit模块
+import streamlit as st
+
+#基于streamlit提供的API来构建Web应用
+st.title("基于streamlit的Web应用")
+st.header("Streamlit 一级标题")
+st.header("Streamlit 二级标题")
+
+#运行程序：streamlit run xxx.py（在终端运行该命令即可启动streamlit服务）
